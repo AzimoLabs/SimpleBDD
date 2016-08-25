f6d150c760ec36e6b3d02f7061969fa0b6b02f20
Przemysław Wośko <przemek.wosko@azimo.com>
2016-08-25 14:44:27 +0200

    silence kif redefined macro warning

--- SimpleBDD/Classes/KIFTestActor+BDDTestScope.h
+++ SimpleBDD/Classes/KIFTestActor+BDDTestScope.h
@@ -10,12 +10,21 @@
 
 #define BDDActorWith(clazz) [clazz actorInFile:[NSString stringWithUTF8String:__FILE__] atLine:__LINE__ delegate:currentScenario.errorHandler]
 
+#undef KIFActorWithClass
 #define KIFActorWithClass(clazz) [clazz actorInFile:[NSString stringWithUTF8String:__FILE__] atLine:__LINE__ delegate:currentScenario.errorHandler]
 
-#define tester BDDActorWith(KIFUITestActor)
+
+#undef viewTester
 #define viewTester BDDActorWith(KIFUIViewTestActor)
+
+#undef tester
+#define tester BDDActorWith(KIFUITestActor)
+
+
 
 #define KIFBDDErrorHandler currentScenario.errorHandler
 #define KIFBDD_CurrentScenario currentScneario
+
+
 
 
